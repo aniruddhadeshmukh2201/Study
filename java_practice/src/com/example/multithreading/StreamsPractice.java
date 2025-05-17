29155e0982d716cdfb9a9a96b78de5407eaa47c9
aniruddhadeshmukh2201 <aniruddhadeshmukh2201@gmail.com>
2025-05-17 16:48:52 +0530

    practiced writing streams

--- java_practice/src/com/example/multithreading/StreamsPractice.java
+++ java_practice/src/com/example/multithreading/StreamsPractice.java
@@ -1,5 +1,31 @@
 package src.com.example.multithreading;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class StreamsPractice {
     
+    
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("a");
+        names.add("b");
+        names.add("c");
+
+        names.stream().forEach((item)-> System.out.println(item));
+
+
+
+        List<String> cities = Arrays.asList("mumbai", "pune", "hingoli");
+        cities.stream().forEach((item) -> System.out.println(item));
+
+        Stream<String> st = Stream.of("x", "y", "z");
+        st.forEach(System.out::println);
+
+        
+
+
+    }
 }
