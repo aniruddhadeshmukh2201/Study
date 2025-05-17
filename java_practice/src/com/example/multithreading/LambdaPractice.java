f93edfd2117d753b3ea9c24a08f8fe18bf6e9d02
aniruddhadeshmukh2201 <aniruddhadeshmukh2201@gmail.com>
2025-05-17 16:26:11 +0530

    lambda coding practice

--- java_practice/src/com/example/multithreading/LambdaPractice.java
+++ java_practice/src/com/example/multithreading/LambdaPractice.java
@@ -1,5 +1,19 @@
 package src.com.example.multithreading;
 
 public class LambdaPractice {
-    
+    static Runnable r = () -> System.out.println("hello");
+
+    static class Task implements Runnable {
+        
+        @Override
+        public void run() {
+            System.out.println("from the inner class");
+        } 
+    }
+    public static void main(String[] args) {
+        r.run();
+
+        new Task().run();
+
+    }
 }
