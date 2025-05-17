44ba2005777dc83762593e03fdf9ffd7ff8824ef
aniruddhadeshmukh2201 <aniruddhadeshmukh2201@gmail.com>
2025-05-17 13:08:32 +0530

    thread class practice

--- java_practice/src/com/example/multithreading/ThreadPractice.java
+++ java_practice/src/com/example/multithreading/ThreadPractice.java
@@ -1,5 +1,19 @@
 package src.com.example.multithreading;
 
 public class ThreadPractice {
-    
+
+    public static class Task extends Thread {
+        @Override
+        public void run() {
+            System.out.println("running using threaed class... " + this.getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> System.out.println("running from the lambda and the thread obj : " + Thread.currentThread().getName()));
+        t1.start();
+        Task t = new Task();
+        t.start();
+        System.out.println("----this is the main thread : " + Thread.currentThread().getName());
+    }
 }
